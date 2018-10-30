17521a88f8fccfd67d2e4d777bdcef7ed5380f7a
GalozhinaYuliya <44209164+GalozhinaYuliya@users.noreply.github.com>
2018-10-30 20:16:33 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,26 +1,69 @@
 #include <iostream>
+#include "NumberLibrary.h"
 
 using namespace std;
 
-int FibonachiNumber();
+int DigitNumber(long long);
+int MaxDigit(long long);
+int MinDigit(long long);
 
-int main(int n)
+int main()
 {
-	int number = FibonachiNumber();
-	cout << "Min Fibonachi number " << number << endl;
+	long long digit;
+	cout << "Enter digit " << endl;
+	cin >> digit;
+    cout << "quantity of numbers " << DigitNumber(digit) << endl;
+	if (IsPrimeNumber(digit))
+	{
+		cout << digit << " -is prime number" << endl;
+	}
+	cout << digit << " - isn't prime number" << endl;
+	cout << "Max number in digit " << MaxDigit(digit) << endl;
+	cout << "Min number in digit " << MinDigit(digit) << endl;
 	system("pause");
-	return 0;
+    return 0;
 }
 
-int FibonachiNumber()
+int DigitNumber(long long digit)
 {
-	int first = 0, n = 0;
-	int second = 1;
-	while (n < 100)
-	{
-		n = first + second;
-		first = second;
-		second = n;
+	
+	int num = 0;
+	while (digit > 0)
+	{ 
+		digit /= 10;
+		num++;
 	}
-	return n;
+	return num;
+}
+int MaxDigit(long long digit)
+{
+	int remainder = 0;
+	int maxdigit = 0;
+	digit = abs(digit);
+	while (digit > 0)
+	{
+		remainder = digit % 10;
+		if (remainder >= maxdigit)
+		{
+			maxdigit = remainder;
+		}
+		digit /= 10;
+	}
+	return maxdigit;
+}
+int MinDigit(long long digit)
+{
+	int remainder = 0;
+	int mindigit = 9;
+	digit = abs(digit);
+	while (digit > 0)
+	{
+		remainder = digit % 10;
+		if (mindigit>=remainder)
+		{
+			mindigit = remainder;
+		}
+		digit /= 10;
+	}
+	return mindigit;
 }
