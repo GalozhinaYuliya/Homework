6b250e16da5c42440fdccb9ea25e5f04addd0750
GalozhinaYuliya <44209164+GalozhinaYuliya@users.noreply.github.com>
2018-11-27 23:33:35 +0300

    Add files via upload

---
+++ DZon14.11.2018/on14,11,2018.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int Zero(int);
+int One(int);
+
+int main()
+{
+	int n;
+	cout << "Please, enter number n " << endl;
+	cin >> n;
+	cout << " Quantity of zero = " << Zero(n) << endl;
+	cout << " Quantity of one = " << One(n) << endl;
+	system("pause");
+}
+int Zero(int n)
+{
+	int zero = 0, a = 0;
+	while (n != 0)
+	{
+		a = n % 2;
+		if (a == 0)
+		{
+			zero += 1;
+		}
+		n /= 2;
+	}
+	return  zero;
+}
+int One(int n)
+{
+	int one = 0, a = 0;
+	while (n != 0)
+	{
+		a = n % 2;
+		if (a == 1)
+		{
+			one += 1;
+		}
+		n /= 2;
+	}
+	return  one;
+}
