1438bf9258b1ddd2529c3195fe43f09c929fcef7
GalozhinaYuliya <44209164+GalozhinaYuliya@users.noreply.github.com>
2019-03-07 13:01:23 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include "ArrayLibrary.h"
+
+typedef bool(*Predicate)(int, int, int);
+typedef int(*Key)(int*, int);
+
+
+using namespace std;
+using namespace ArrayExtension;
+
+void Array(int *, int);
+void displayArray(int*, int);
+int GetKeys(int*, int);
+bool predicate(int, int, int);
+void DeleteElements(int*, int&, int);
+void NewArray(int*, int&, int*, Predicate);
+int GetAmountOfDeletedElements(int*, int, Key, Predicate);
+
+int main()
+{
+	int n;
+	while (true)
+	{
+		cout << "Enter n ";
+		cin >> n;
+		if (n > 0)
+		{
+			break;
+		}
+		cout << "Invalid data" << endl;
+	}
+
+	int* firstmassive = new int[n];//вводили 5 элементов, минимумом брала 2, элемнтами были 2 4 7 9 5, в новый массив записывались 7 , удал€€сь при этом из старого
+    Array(firstmassive, n);
+	displayArray(firstmassive, n);
+
+	
+
+	int m = GetAmountOfDeletedElements(firstmassive, n, GetKeys, predicate);
+
+	int* secondmassive = new int[m];
+	NewArray(firstmassive, n, secondmassive, predicate);
+
+	displayArray(secondmassive, m);
+	cout << endl;
+	displayArray(firstmassive, n);
+	cout << endl;
+	system("pause");
+}
+void Array(int* firstmassive, int n)
+{
+	for (int* j = firstmassive, i = 0; j < firstmassive + n; j++, i++)
+	{
+		cout << "[ " << i + 1 << " ] = ";
+		cin >> *j;
+
+	}
+}
+
+void displayArray(int* firstmassive, int n)
+{
+	for (int* j = firstmassive; j < firstmassive + n; j++)
+	{
+		cout << *j << "  ";
+	}
+	cout << endl;
+}
+
+int GetKeys(int* firstmassive, int n)
+{
+	int min = firstmassive[0];
+
+	for (int i = 1; i < n; i++)
+	{
+		if (firstmassive[i] < min)
+		{
+			min = firstmassive[i];
+		}
+	}
+
+	return min;
+}
+
+bool predicate(int a, int b,int min)
+{
+		return a % min == 0 && b % min != 0;
+}
+
+void NewArray(int* firstmassive, int& n, int* secondmassive, Predicate predicate)
+{
+	int min = GetKeys(firstmassive, n);
+
+	for (int i = 0, j = 0; i < n; i++)
+	{
+		if (predicate(firstmassive[i],firstmassive[i + 1], min))
+		{
+			secondmassive[j] = firstmassive[i + 1];
+			j++;
+			DeleteElements(firstmassive, n, i + 2);
+
+		}
+	
+	}
+}
+
+void DeleteElements(int* array, int& n, int index)
+{
+	int m = n;
+	for (int i = index - 1; i < m; i++)
+	{
+		array[i] = array[i + 1];
+	}
+
+	n--;
+}
+
+int GetAmountOfDeletedElements(int* array, int n,Key key, Predicate predicate)
+{
+	int i = 0, k = 0;
+
+	while (i < n)
+	{
+		if (predicate(array[i], array[i+1],key(array,n)))
+		{
+			k++;
+		}
+
+		i++;
+	}
+
+	return k;
+}
